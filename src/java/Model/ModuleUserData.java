6b04ee2418384a757002982ad10f525e482cac30
Andree Yosua <andree.yosua@gmail.com>
2017-04-28 20:03:02 +1000

    Update javadoc for ModuleUserData.java

--- src/java/Model/ModuleUserData.java
+++ src/java/Model/ModuleUserData.java
@@ -14,11 +14,11 @@ public class ModuleUserData {
     private String mValue;
 
     /**
-     *
-     * @param moduleID
-     * @param userID
-     * @param mKey
-     * @param mValue
+     * Class to represent variety user data from <code>Module</code>.
+     * @param moduleID Module ID
+     * @param userID User ID
+     * @param mKey Key data
+     * @param mValue Value of key data
      */
     public ModuleUserData(int moduleID, int userID, String mKey, String mValue) {
         this.moduleID = moduleID;
@@ -27,72 +27,74 @@ public class ModuleUserData {
         this.mValue = mValue;
     }
 
+    // <editor-fold defaultstate="collapsed" desc="Getter and Setter. Click + sign on the left to expand the code">
     /**
-     *
-     * @return
+     * Return the <code>Module</code> ID tied to this <code>ModuleUserData</code> in <code>int</code> format.
+     * @return Module ID.
      */
     public int getModuleID() {
         return moduleID;
     }
 
     /**
-     *
-     * @param moduleID
+     * Set the <code>Module</code> ID tied to this <code>ModuleUserData</code> to specified <code>int</code>.
+     * @param moduleID Module new ID
      */
     public void setModuleID(int moduleID) {
         this.moduleID = moduleID;
     }
 
     /**
-     *
-     * @return
+     * Return the <code>User</code> ID tied to this <code>ModuleUserData</code> in <code>int</code> format.
+     * @return User ID.
      */
     public int getUserID() {
         return userID;
     }
 
     /**
-     *
-     * @param userID
+     * Set the <code>User</code> ID tied to this <code>ModuleUserData</code> to specified <code>int</code>.
+     * @param userID User new ID
      */
     public void setUserID(int userID) {
         this.userID = userID;
     }
 
     /**
-     *
-     * @return
+     * Return the key of <code>ModuleUserData</code> in <code>String</code> format.
+     * @return Key.
      */
     public String getmKey() {
         return mKey;
     }
 
     /**
-     *
-     * @param mKey
+     * Set the <code>ModuleUserData</code> key to specified <code>String</code>.
+     * @param mKey New key data
      */
     public void setmKey(String mKey) {
         this.mKey = mKey;
     }
 
     /**
-     *
-     * @return
+     * Return the key value of <code>ModuleUserData</code> in <code>String</code> format.
+     * @return Value of key
      */
     public String getmValue() {
         return mValue;
     }
 
     /**
-     *
-     * @param mValue
+     * Set the <code>ModuleUserData</code> key value to specified <code>String</code>.
+     * @param mValue New value of key
      */
     public void setmValue(String mValue) {
         this.mValue = mValue;
-    }
+    }// </editor-fold>\
 
     /**
-     *
+     * Sort <code>ModuleUserData</code> by <code>mValue</code> descending for high score.
+     * For this Comparator, <code>mValue</code> must contain number only as the <code>mValue</code> would be converted to <code>int</code> for sorting.
      */
     public static Comparator<ModuleUserData> sortByScoreDesc = new Comparator<ModuleUserData>() {
         @Override
@@ -102,7 +104,7 @@ public class ModuleUserData {
     };
 
     /**
-     *
+     * Sort <code>ModuleUserData</code> by <code>moduleID</code>  ascending for listing user library.
      */
     public static Comparator<ModuleUserData> sortByModuleIDAsc = new Comparator<ModuleUserData>() {
         @Override
